de36b2ba3ff27f1b22c75e36a840cd95bd4c01fe
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-06-11 01:05:45 +0900

    :hammer: :: add nanoId

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,10 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+
+    // nanoid
+    implementation ("io.viascom.nanoid:nanoid:1.0.1")
 }
 
 kotlin {
