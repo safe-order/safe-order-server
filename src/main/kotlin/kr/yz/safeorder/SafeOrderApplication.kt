2a50993fa0eddb99dbf037501b80732f29d5c613
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-06-10 19:50:30 +0900

    :tada: :: create project

---
+++ src/main/kotlin/kr/yz/safeorder/SafeOrderApplication.kt
@@ -0,0 +1,11 @@
+package kr.yz.safeorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SafeOrderApplication
+
+fun main(args: Array<String>) {
+    runApplication<SafeOrderApplication>(*args)
+}
---
+++ src/test/kotlin/kr/yz/safeorder/SafeOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.yz.safeorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SafeOrderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
