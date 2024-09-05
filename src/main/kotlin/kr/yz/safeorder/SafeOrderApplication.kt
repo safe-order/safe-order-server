1770ce03e223f27338af8d5b76a48a5cde356b54
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-09-06 00:10:38 +0900

    :zap: :: set JwtProperties

--- src/main/kotlin/kr/yz/safeorder/SafeOrderApplication.kt
+++ src/main/kotlin/kr/yz/safeorder/SafeOrderApplication.kt
@@ -1,9 +1,12 @@
 package kr.yz.safeorder
 
+import kr.yz.safeorder.global.security.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties::class)
 class SafeOrderApplication
 
 fun main(args: Array<String>) {
