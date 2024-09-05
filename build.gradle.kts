5509ff01971be3809e1085aa5a170577c3d33295
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-09-06 00:15:34 +0900

    :hammer: :: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
-    id ("org.jetbrains.kotlin.plugin.allopen") version "1.9.22"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.22"
 
     kotlin("plugin.jpa") version "1.9.24"
     kotlin("jvm") version "1.9.24"
@@ -25,19 +25,36 @@ repositories {
     mavenCentral()
 }
 
+val jjwtVersion = "0.11.5"
+val springDocOpenApiVersion = "1.7.0"
+val coroutinesVersion = "1.8.1"
+
 dependencies {
+    implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
+    implementation("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
+    implementation("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$coroutinesVersion")
+    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+    implementation("org.eclipse.jetty:jetty-reactive-httpclient:1.1.4")
+
+    implementation ("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // nanoid
-    implementation ("io.viascom.nanoid:nanoid:1.0.1")
+    implementation("io.viascom.nanoid:nanoid:1.0.1")
 }
 
 kotlin {
